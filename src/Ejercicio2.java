ab17ff9ac2e7698ce09dfd3363ad11b1bf9f3c80
RVS <m1lky.way.set@gmail.com>
2025-09-04 16:25:22 -0400

    añadido cuatro del nueve

---
+++ src/Ejercicio2.java
@@ -0,0 +1,20 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+
+
+
+        String rut = "12345678-9";
+        System.out.println("Devuelve la subcadena de un texto");
+        System.out.println(rut.substring(9,10));
+
+
+        System.out.println("El rut es: " + rut);
+
+
+
+
+
+
+    }
+}
---
+++ src/Ejercicio3.java
@@ -0,0 +1,36 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+
+        int[] numeros = new int[5];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        System.out.println("for tradicional");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(numeros[i]);
+         }
+        System.out.println("    ");
+            int[] num = {5,4,3,2,1};
+            for (int n = 0; n < 5; n++) {
+                System.out.println(numeros[n]);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
